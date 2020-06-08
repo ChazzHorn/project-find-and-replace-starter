19b10018a3a4e96b7df816c645428a4c561ccad6
Chazz Horn <Chazzhorn1@gmail.com>
2020-06-08 16:35:45 -0400

    added nested loop

--- main.js
+++ main.js
@@ -9,18 +9,63 @@ const replaceAllButton = document.querySelector(".replace-all-button")
 // Later you will need an OUTER LOOP to loop over the individual elements within
 // this array.
 const rowElements = document.querySelectorAll(".row")
-
-// When you call the function belwo, it will get and return an INNER ARRAY
+console.log(rowElements)
+// When you call the function below, it will get and return an INNER ARRAY
 // containing the cell elements for a given row.
 // Call this function from WITHIN your row elements loop. Then you will, in turn,
 // need to loop over the resulting cell elements. But where should this whole
 // NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
+
+
+
+
 function getCellElements (currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
 }
-
-
+//a loop to find all that we are looking for & a loop to replace what we find with given content.
 // YOUR CODE GOES HERE
+
+
+replaceAllButton.addEventListener('click', function(){
+    let findInputValue= findInput.value
+    let replaceInputValue= replaceInput.value
+
+    for (rowfinder=0; rowfinder<rowElements.length; rowfinder++){
+
+        let nodeListElements = getCellElements(rowElements[rowfinder])
+            console.log(nodeListElements)
+
+        for(cellfinder=0; cellfinder<nodeListElements.length; cellfinder++){
+
+            let currentCell= nodeListElements[cellfinder]
+            let textInsideDiv = currentCell.innerHTML
+
+            console.log(textInsideDiv, findInputValue)
+
+            if(textInsideDiv.includes(findInputValue)){
+
+                let result = textInsideDiv.replace(findInputValue,replaceInputValue)
+
+                textInsideDiv = result
+
+                console.log(textInsideDiv)
+            }
+            
+            // console.log(replaceInput)
+            }
+    }
+    // console.log('I\'m Workin')
+})
+
+
+
+
+
+
+
+
+
+
 
 
 // One last thing: dedicate very careful attention to using variables and
